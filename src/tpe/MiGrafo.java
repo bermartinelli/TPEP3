7a2fd02cdff54a171e8c77fb6f35df7ec42e049a
Miqueas Freiberger <miqueasfreiberger@gmail.com>
2023-05-27 16:59:57 -0300

    fix bug en servicio caminos y agregue uso de lim

--- src/tpe/MiGrafo.java
+++ src/tpe/MiGrafo.java
@@ -4,87 +4,81 @@ import java.util.Iterator;
 import java.util.List;
 
 public class MiGrafo {
-	
-	 public static void main (String [] args){
-		 GrafoDirigido miGrafo = new GrafoDirigido();
-			
-			ServicioBFS bfs = new ServicioBFS(miGrafo);
-			ServicioDFS dfs = new ServicioDFS(miGrafo);
-			
-			miGrafo.agregarVertice(1);
-			miGrafo.agregarVertice(2);
-			miGrafo.agregarVertice(3);
-			miGrafo.agregarVertice(4);
-			miGrafo.agregarVertice(5);
-			
-			
-			miGrafo.agregarArco(1 , 5, "1-5");
-			miGrafo.agregarArco(4 , 1, "4-1");
-			miGrafo.agregarArco(1 , 2, "1-2");
-			miGrafo.agregarArco(2 , 3, "2-3");
-			miGrafo.agregarArco(4 , 3, "4-3");
-			miGrafo.agregarArco(4 , 5, "4-5");
-			miGrafo.agregarArco(5 , 4, "5-4");
-			
-			System.out.println(miGrafo.existeArco(1, 5));
-			System.out.println(miGrafo.existeArco(2, 3));
-			System.out.println(miGrafo.existeArco(4, 3));
-			
-			
-			System.out.println();
-			System.out.println(miGrafo.existeArco(1, 5));
-			System.out.println(miGrafo.existeArco(1, 2));
 
-			
-			System.out.println("Cantidad de vertices: " + miGrafo.cantidadVertices());
-			
-			
-			
-			Iterator<Integer> vertices = miGrafo.obtenerVertices();
-			int primero = vertices.next();
-				
-			System.out.println("primero: " + primero);
-			
-			
-			Iterator it = miGrafo.obtenerAdyacentes(1);
-			while(it.hasNext()) {
-				System.out.println(it.next());
+	public static void main(String[] args) {
+		GrafoDirigido miGrafo = new GrafoDirigido();
+
+		ServicioBFS bfs = new ServicioBFS(miGrafo);
+		ServicioDFS dfs = new ServicioDFS(miGrafo);
+
+		miGrafo.agregarVertice(1);
+		miGrafo.agregarVertice(2);
+		miGrafo.agregarVertice(3);
+		miGrafo.agregarVertice(4);
+		miGrafo.agregarVertice(5);
+
+		miGrafo.agregarArco(1, 5, "1-5");
+		miGrafo.agregarArco(4, 1, "4-1");
+		miGrafo.agregarArco(1, 2, "1-2");
+		miGrafo.agregarArco(2, 3, "2-3");
+		miGrafo.agregarArco(4, 3, "4-3");
+		miGrafo.agregarArco(4, 5, "4-5");
+		miGrafo.agregarArco(5, 4, "5-4");
+
+		System.out.println(miGrafo.existeArco(1, 5));
+		System.out.println(miGrafo.existeArco(2, 3));
+		System.out.println(miGrafo.existeArco(4, 3));
+
+		System.out.println();
+		System.out.println(miGrafo.existeArco(1, 5));
+		System.out.println(miGrafo.existeArco(1, 2));
+
+		System.out.println("Cantidad de vertices: " + miGrafo.cantidadVertices());
+
+		Iterator<Integer> vertices = miGrafo.obtenerVertices();
+		int primero = vertices.next();
+
+		System.out.println("primero: " + primero);
+
+		Iterator it = miGrafo.obtenerAdyacentes(1);
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+		System.out.println("--");
+
+		List<Integer> listaBFS = bfs.bfsForest();
+
+		System.out.println("Lista BFS");
+		for (Integer vertice : listaBFS) {
+			System.out.println(vertice);
+		}
+
+		System.out.println("--");
+
+		List<Integer> listaDFS = dfs.dfsForest();
+
+		System.out.println("Lista DFS");
+		for (Integer vertice : listaDFS) {
+			System.out.println(vertice);
+		}
+
+		ServicioCaminos caminos = new ServicioCaminos(miGrafo, 1, 3, 2);
+
+		List<List<Integer>> listaDeCaminos = caminos.caminos();
+
+		System.out.println("Cantidad de caminos: " + listaDeCaminos.size());
+
+		int contador = 0;
+		for (List<Integer> camino : listaDeCaminos) {
+			contador++;
+			System.out.print("Camino " + contador + " : ");
+			for (Integer vertice : camino) {
+				System.out.print(vertice + ", ");
 			}
-			
-			System.out.println("--");
-			
-			List<Integer> listaBFS = bfs.bfsForest();
-			
-			System.out.println("Lista BFS");
-			for(Integer vertice : listaBFS) {
-				System.out.println(vertice);
-			}
-			
-			System.out.println("--");
-			
-			List<Integer> listaDFS = dfs.dfsForest();
-			
-			System.out.println("Lista DFS");
-			for(Integer vertice : listaDFS) {
-				System.out.println(vertice);
-			}
-			
-			ServicioCaminos caminos =  new ServicioCaminos(miGrafo, 1, 3, 1);
-			
-			List<List<Integer>> listaDeCaminos = caminos.caminos();
-			
-			System.out.println("Cantidad de caminos: " + listaDeCaminos.size());
-			
-			int contador = 0;
-			for (List<Integer> camino : listaDeCaminos) {
-				contador++;
-				System.out.print("Camino " + contador + " : ");
-				for(Integer vertice : camino) {
-					System.out.print(vertice + ", ");
-				}
-			}
-			
-	 }
-	
-	
+			System.out.println(); // Agregar un salto de línea al final de cada camino impreso
+		}
+
+	}
+
 }
--- src/tpe/ServicioCaminos.java
+++ src/tpe/ServicioCaminos.java
@@ -6,51 +6,41 @@ import java.util.List;
 
 
 public class ServicioCaminos {
-
-	private Grafo<?> grafo;
-	private int origen;
-	private int destino;
-	private int lim;
-	
-	// Servicio caminos
-	public ServicioCaminos(Grafo<?> grafo, int origen, int destino, int lim) {
-		this.grafo = grafo;
-		this.origen = origen;
-		this.destino = destino;
-		this.lim = lim;
-	}
-
-	public List<List<Integer>> caminos() {
-		List<List<Integer>> resultado = new ArrayList<>();
-		List<Integer> camino = new ArrayList<>();
-		
-		camino.add(origen);
-		Iterator<Integer> vertices = grafo.obtenerAdyacentes(origen);
-		
-		while(vertices.hasNext()) {
-			Integer actual = vertices.next();
-			List<Integer> parcial = camino;
-			parcial.add(actual);
-			caminos(actual, destino, parcial, resultado);
-		}
-		
-		return resultado;
-	}
-	
-	public void caminos(Integer origen, Integer destino, List<Integer> camino, List<List<Integer>> res) {
-		if(origen == destino) {
-			res.add(camino);
-		} else {
-			Iterator<Integer> vertices = grafo.obtenerAdyacentes(origen);
-			while(vertices.hasNext()) {
-				Integer actual = vertices.next();
-				List<Integer> parcial = camino;
-				if(!parcial.contains(actual)) {
-					parcial.add(actual);
-					caminos(actual, destino, parcial, res);
-				}
-			}
-		}
-	}
-
+    private Grafo<?> grafo;
+    private int origen;
+    private int destino;
+    private int lim;
+    
+    public ServicioCaminos(Grafo<?> grafo, int origen, int destino, int lim) {
+        this.grafo = grafo;
+        this.origen = origen;
+        this.destino = destino;
+        this.lim = lim;
+    }
+    
+    public List<List<Integer>> caminos() {
+        List<List<Integer>> resultado = new ArrayList<>();
+        List<Integer> camino = new ArrayList<>();
+        
+        camino.add(origen);
+        caminos(origen, destino, camino, resultado, 0); // Llamar al método caminos con un contador de arcos igual a 0
+        
+        return resultado;
+    }
+    
+    private void caminos(Integer origen, Integer destino, List<Integer> camino, List<List<Integer>> res, int arcos) {
+        if (origen.equals(destino) && arcos <= lim) { // Verificar que el origen sea igual al destino y que no se hayan superado el límite de arcos
+            res.add(camino);
+        } else if (arcos < lim) { // Verificar que no se hayan superado el límite de arcos
+            Iterator<Integer> vertices = grafo.obtenerAdyacentes(origen);
+            while (vertices.hasNext()) {
+                Integer actual = vertices.next();
+                List<Integer> parcial = new ArrayList<>(camino); // Crear una nueva lista para cada camino parcial
+                if (!parcial.contains(actual)) {
+                    parcial.add(actual);
+                    caminos(actual, destino, parcial, res, arcos + 1); // Incrementar el contador de arcos en cada llamada recursiva
+                }
+            }
+        }
+    }
 }
